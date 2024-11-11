e2ee1c51009069f2a6180108aafeea31db7ad40c
Allen Wild <allenwild93@gmail.com>
2024-11-11 18:35:05 -0500

    add logic to filter variables
    Patterns can be regexes or globs, a variable is included if it matches
    any pattern. By default, patterns are case-insensitive regexes.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+use std::ffi::OsStr;
 use std::io::{self, Read, Write};
+use std::os::unix::ffi::OsStrExt;
 
 use anstyle::{AnsiColor, Style};
 use anyhow::Context as _;
 use clap::Parser;
+use globset::{GlobBuilder, GlobSet};
+use regex::bytes::{RegexSet, RegexSetBuilder};
 
 const STYLE_KEY: Style = color_style(AnsiColor::Green);
 const STYLE_EQU: Style = color_style(AnsiColor::Blue);
@@ -12,7 +16,23 @@ const fn color_style(color: AnsiColor) -> Style {
     Style::new().fg_color(Some(anstyle::Color::Ansi(color)))
 }
 
-fn print_env<W: Write>(buf: &[u8], out: &mut W) -> io::Result<()> {
+enum Pattern {
+    Empty,
+    Glob(GlobSet),
+    Regex(RegexSet),
+}
+
+impl Pattern {
+    fn is_match(&self, name: &[u8]) -> bool {
+        match self {
+            Self::Empty => true,
+            Self::Glob(globs) => globs.is_match(OsStr::from_bytes(name)),
+            Self::Regex(regexes) => regexes.is_match(name),
+        }
+    }
+}
+
+fn print_env<W: Write>(buf: &[u8], out: &mut W, pattern: &Pattern) -> io::Result<()> {
     for chunk in buf.split(|b| *b == b'\0') {
         if chunk.is_empty() {
             continue;
@@ -28,6 +48,10 @@ fn print_env<W: Write>(buf: &[u8], out: &mut W) -> io::Result<()> {
             // a zero-length array, so then we have to index it to actually get a slice.
             None => (chunk, &[][..]),
         };
+
+        if !pattern.is_match(key) {
+            continue;
+        }
 
         // [u8] isn't Display so do it ourselves
         STYLE_KEY.write_to(out)?;
@@ -55,10 +79,25 @@ struct Args {
     #[arg(short, long, requires = "file")]
     pid: bool,
 
+    /// PATTERN is a glob instead of regex
+    #[arg(short, long, requires = "pattern")]
+    glob: bool,
+
+    /// PATTERN is case-sensitive
+    #[arg(short = 's', long, requires = "pattern")]
+    case_sensitive: bool,
+
     /// File path, omit or specify '-' to read stdin.
     ///
     /// When using --pid, this is a process ID number
     file: Option<String>,
+
+    /// Filter the variable names based on patterns
+    ///
+    /// Include variables matching any pattern in this list. By default, patterns are
+    /// case-insensitive regexes, but this can be modified using the -g/--glob and
+    /// -s/--case-sensitive flags.
+    pattern: Option<Vec<String>>,
 }
 
 fn run() -> anyhow::Result<()> {
@@ -77,6 +116,27 @@ fn run() -> anyhow::Result<()> {
         }
     };
 
+    let pattern = match (args.pattern, args.glob) {
+        (None, _) => Pattern::Empty,
+        (Some(ref pats), true) => {
+            let mut builder = GlobSet::builder();
+            for pat in pats {
+                builder.add(
+                    GlobBuilder::new(pat)
+                        .case_insensitive(!args.case_sensitive)
+                        .build()
+                        .with_context(|| format!("invalid glob '{pat}'"))?,
+                );
+            }
+            Pattern::Glob(builder.build().context("failed to build GlobSet")?)
+        }
+        (Some(ref pats), false) => {
+            let mut builder = RegexSetBuilder::new(pats);
+            builder.case_insensitive(!args.case_sensitive);
+            Pattern::Regex(builder.build().context("failed to build RegexSet")?)
+        }
+    };
+
     let data = match &path {
         Some(path) => std::fs::read(path).with_context(|| format!("failed to read {path}"))?,
         None => {
@@ -90,7 +150,7 @@ fn run() -> anyhow::Result<()> {
     };
 
     let mut out = anstream::stdout().lock();
-    print_env(&data, &mut out)?;
+    print_env(&data, &mut out, &pattern)?;
 
     Ok(())
 }
